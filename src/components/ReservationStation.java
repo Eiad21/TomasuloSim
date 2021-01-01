ff3e2658d29fffa08b256556cade1f5db5bbd34d
Eiad21 <eiadessam1@gmail.com>
2021-01-01 13:07:29 +0200

    first commit

---
+++ src/components/ReservationStation.java
@@ -0,0 +1,34 @@
+package components;
+
+import utility.OP;
+
+public class ReservationStation {
+	private boolean busy;
+	private OP op;
+	private double vJ, vK;
+	private ReservationStation qJ, qK;
+	private int A;
+	private int timeRemExec;
+	
+	public ReservationStation() {
+		
+	}
+	
+	public boolean isBusy() {
+		return busy;
+	}
+	
+	public void issueInst(OP op, int vJ, int vK, ReservationStation qJ, ReservationStation qK) {
+		this.op = op;
+		this.vJ = vJ;
+		this.vK = vK;
+		this.qJ = qJ;
+		this.qK = qK;
+		timeRemExec = op.execTime();
+		busy = true;
+	}
+	
+	public void flushInst(){
+		busy = false;
+	}
+}
